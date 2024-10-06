17a892091c360a3640b5616be0505f7295834aee
Maaroufabousaleh <143460437+Maaroufabousaleh@users.noreply.github.com>
2024-10-05 22:58:36 -0400

    Update Server.java

--- INF3405_tp1/src/server_packages/Server.java
+++ INF3405_tp1/src/server_packages/Server.java
@@ -1,17 +1,45 @@
-package server_packages;
-
 import java.io.IOException;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
+import java.util.regex.Pattern;
 
 public class Server {
-    private static ServerSocket Listener; // Application Serveur
+	
+    private static final Pattern IP_PATTERN = 
+		Pattern.compile("^(([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.){3}([01]?\\d\\d?|2[0-4]\\d|25[0-5])$"); // https://stackoverflow.com/questions/5667371/validate-ipv4-address-in-java
+
+    public static boolean isValidIP(final String ip) {
+        return IP_PATTERN.matcher(ip).matches();
+    }
+	
+	public static boolean isValidPort(final int port) {
+		if (port >= 5000 && port <= 5500){
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	
+    private static ServerSocket Listener; 
 
     public static void main(String[] args) {
-        int clientNumber = 0; // Compteur incrémenté à chaque connexion d'un client au serveur
-        String serverAddress = "127.0.0.1"; // Adresse du serveur
-        int serverPort = 5000; // Port du serveur
+        int clientNumber = 0; 
+        
+        System.out.println("Veuillez entrez l'addresse IP de votre serveur: ");
+        String serverAddress = System.console().readLine();
+        while (!Server.isValidIP(serverAddress)){
+            System.out.println("Adresse IP invalid! Veuillez saisir une valeur de forme x.x.x.x avec  une taille de 1 octet: ");
+        	serverAddress = System.console().readLine();
+        }
+        
+        System.out.println("Veuillez entrez l'addresse du port de votre serveur: ");
+        int serverPort = Integer.parseInt(System.console().readLine());
+        while (!Server.isValidPort(serverPort)){
+            System.out.println("Port invalid! Veuillez saisir une valeur entre 5000 et 5500: ");
+            serverPort = Integer.parseInt(System.console().readLine());
+        }
         
         try {
             // Création de la connexion pour communiquer avec les clients
